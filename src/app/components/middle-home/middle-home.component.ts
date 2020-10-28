4a042d24342c78d7a7937290b19d396c639b1dac
clf94-dev <clf94@hotmail.es>
2020-10-28 16:41:38 +0100

    added middleHome component (struct and style)

--- src/app/components/middle-home/middle-home.component.ts
+++ src/app/components/middle-home/middle-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MiddleHomeData } from './middleHomeData';
 
 @Component({
   selector: 'app-middle-home',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./middle-home.component.scss']
 })
 export class MiddleHomeComponent implements OnInit {
-
-  constructor() { }
+  public middledata: MiddleHomeData [];
+  constructor() { 
+    this.middledata=[
+      new MiddleHomeData ('Easy to implement','Our API comes with just a few lines of code. You’ll be up and running in no time. We built our documentation page to integrate payments functionality with ease.','Easy to implement','reverse','../../../assets/images/home/desktop/illustration-easy-to-implement.svg'),
+      new MiddleHomeData ('Simple UI & UX','Our pre-built form is easy to integrate in your app or website’s checkout flow and designed to optimize conversion. ','Simple UI & UX','','../../../assets/images/home/desktop/illustration-simple-ui.svg')
+    ];
+  }
 
   ngOnInit(): void {
   }
---
+++ src/app/components/middle-home/middleHomeData.ts
@@ -0,0 +1,8 @@
+export class MiddleHomeData {
+ constructor ( 
+     public title: string,
+   public text: string,
+   public imgAlt: string,
+   public classes: string,
+   public src: string) {}
+}
