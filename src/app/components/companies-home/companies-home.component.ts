9e7d3269665bcf0cb76860cd3487bb172936e773
clf94-dev <clf94@hotmail.es>
2020-10-28 16:36:47 +0100

    added companiesHome (struct and styles)

--- src/app/components/companies-home/companies-home.component.ts
+++ src/app/components/companies-home/companies-home.component.ts
@@ -6,7 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./companies-home.component.scss']
 })
 export class CompaniesHomeComponent implements OnInit {
-
+  google='../../../assets/images/shared/desktop/google.svg'
+  microsoft='../../../assets/images/shared/desktop/microsoft.svg'
+  hp='../../../assets/images/shared/desktop/hewlett-packard.svg'
+  oracle='../../../assets/images/shared/desktop/oracle.svg'
+  tesla='../../../assets/images/shared/desktop/tesla.svg'
+  nVidia='../../../assets/images/shared/desktop/nvidia.svg'
+  
+  
   constructor() { }
 
   ngOnInit(): void {
