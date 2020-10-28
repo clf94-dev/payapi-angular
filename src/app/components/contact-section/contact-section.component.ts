7cf79336a92c5af697ccdb0d695fd769e80b34fc
clf94-dev <clf94@hotmail.es>
2020-10-28 17:49:43 +0100

    added contactSection component struct

--- src/app/components/contact-section/contact-section.component.ts
+++ src/app/components/contact-section/contact-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LogosInfo } from './logosInfo';
 
 @Component({
   selector: 'app-contact-section',
@@ -6,8 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-section.component.scss']
 })
 export class ContactSectionComponent implements OnInit {
+  public logos: LogosInfo[];
+  constructor() {
+    this.logos =[
+      new LogosInfo("../../../assets/images/shared/desktop/google.svg", "Google"),
+      new LogosInfo("../../../assets/images/shared/desktop/hewlett-packard.svg", "Hp"),
+      new LogosInfo("../../../assets/images/shared/desktop/microsoft.svg", "Microsoft"),
+      new LogosInfo("../../../assets/images/shared/desktop/tesla.svg", "Tesla"),
+      new LogosInfo("../../../assets/images/shared/desktop/oracle.svg", "Oracle"),
+      new LogosInfo("../../../assets/images/shared/desktop/nvidia.svg", "NVidia")
 
-  constructor() { }
+    ]
+   }
 
   ngOnInit(): void {
   }
---
+++ src/app/components/contact-section/logosInfo.ts
@@ -0,0 +1,6 @@
+export class LogosInfo {
+    constructor(
+        public src: string,
+        public imgAlt: string
+    ){}
+}
